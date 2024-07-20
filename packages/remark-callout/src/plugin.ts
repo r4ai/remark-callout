9e8bf96fd9a9e63f3f3e92c43a2ec3211e241a26
rai <mj82xx@gmail.com>
2024-07-21 00:17:10 +0900

    docs: add jsdoc to options.foldIcon

--- packages/remark-callout/src/plugin.ts
+++ packages/remark-callout/src/plugin.ts
@@ -78,6 +78,31 @@ export type OptionsBuilder<N> = {
    */
   icon?: Optional<WithChildren<N>>;
 
+  /**
+   * The fold icon node of the callout.
+   *
+   * The fold icon node is added in the title node after the title text.
+   *
+   * - If `undefined`, no fold icon is added.
+   * - If a `string`, the string is added as HTML in the title node after the title text.
+   * - If a `object`, the object is added as a node after the title text.
+   *
+   * @example
+   * (callout) =>
+   *   callout.isFoldable
+   *     ? {
+   *         tagName: "div",
+   *         properties: {
+   *           className: "callout-fold-icon",
+   *         },
+   *         children:
+   *           '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>', // lucide:chevron-right
+   *       }
+   *     : undefined,
+   *
+   * @default
+   * () => undefined
+   */
   foldIcon?: Optional<WithChildren<N>>;
 
   /**
