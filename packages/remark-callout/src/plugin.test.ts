59ff4ef535be9a472be3dcf14e0b50494fffe8fe
rai <mj82xx@gmail.com>
2024-07-21 00:09:52 +0900

    test: remove unnecessary ast

--- packages/remark-callout/src/plugin.test.ts
+++ packages/remark-callout/src/plugin.test.ts
@@ -464,34 +464,27 @@ describe("remarkCallout", () => {
         children: [
           {
             type: "element",
-            tagName: "body",
-            properties: {},
+            tagName: "svg",
+            properties: {
+              className: "lucide-pencil",
+              xmlns: "http://www.w3.org/2000/svg",
+              width: "32",
+              height: "32",
+              viewBox: "0 0 24 24",
+            },
             children: [
               {
                 type: "element",
-                tagName: "svg",
+                tagName: "path",
                 properties: {
-                  className: "lucide-pencil",
-                  xmlns: "http://www.w3.org/2000/svg",
-                  width: "32",
-                  height: "32",
-                  viewBox: "0 0 24 24",
+                  fill: "none",
+                  stroke: "#888888",
+                  strokeLineCap: "round",
+                  strokeLineJoin: "round",
+                  strokeWidth: "2",
+                  d: "M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497zM15 5l4 4",
                 },
-                children: [
-                  {
-                    type: "element",
-                    tagName: "path",
-                    properties: {
-                      fill: "none",
-                      stroke: "#888888",
-                      strokeLineCap: "round",
-                      strokeLineJoin: "round",
-                      strokeWidth: "2",
-                      d: "M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497zM15 5l4 4",
-                    },
-                    children: [],
-                  },
-                ],
+                children: [],
               },
             ],
           },
