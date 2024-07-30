ca1e884790446751e1d536e44827c3fa111eb4db
rai <96561881+r4ai@users.noreply.github.com>
2024-07-30 19:06:16 +0900

    fix: skip empty callout body (#146)
    * chore: skip empty callout body
    
    * test: remove console.log
    
    * chore: changeset

---
+++ .changeset/curly-maps-collect.md
@@ -0,0 +1,5 @@
+---
+"@r4ai/remark-callout": minor
+---
+
+Don't render callout body when it is empty.
--- packages/remark-callout/src/plugin.test.ts
+++ packages/remark-callout/src/plugin.test.ts
@@ -242,16 +242,14 @@ describe("remarkCallout", () => {
     expect(callout?.getAttribute("data-callout-type")).toBe("note");
     expect(callout?.tagName.toLowerCase()).toBe("div");
     expect(callout?.getAttribute("open")).toBe(null);
-    expect(callout?.children.length).toBe(2);
+    expect(callout?.children.length).toBe(1);
 
     const calloutTitle = callout?.querySelector("[data-callout-title]");
     expect(calloutTitle).not.toBe(null);
     expect(calloutTitle?.textContent).toBe("title here");
 
     const calloutBody = callout?.querySelector("[data-callout-body]");
-    expect(calloutBody).not.toBe(null);
-    expect(calloutBody?.textContent?.trim()).toBe("");
-    expect(calloutBody?.children.length).toBe(0);
+    expect(calloutBody).toBe(null);
   });
 
   test("callout without title and body", async () => {
@@ -267,16 +265,14 @@ describe("remarkCallout", () => {
     expect(callout?.getAttribute("data-callout-type")).toBe("note");
     expect(callout?.tagName.toLowerCase()).toBe("div");
     expect(callout?.getAttribute("open")).toBe(null);
-    expect(callout?.children.length).toBe(2);
+    expect(callout?.children.length).toBe(1);
 
     const calloutTitle = callout?.querySelector("[data-callout-title]");
     expect(calloutTitle).not.toBe(null);
     expect(calloutTitle?.textContent).toBe("Note");
 
     const calloutBody = callout?.querySelector("[data-callout-body]");
-    expect(calloutBody).not.toBe(null);
-    expect(calloutBody?.textContent?.trim()).toBe("");
-    expect(calloutBody?.children.length).toBe(0);
+    expect(calloutBody).toBe(null);
   });
 
   test("callout with title consisting of multiple nodes", async () => {
